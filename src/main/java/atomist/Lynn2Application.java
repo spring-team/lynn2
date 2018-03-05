faf5f911bf7f8ce057764f4dff62e6eddb5b8123
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 20:35:59 +0000

    Initial commit from Atomist

---
+++ src/main/java/atomist/Lynn2Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lynn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lynn2Application.class, args);
+	}
+}
